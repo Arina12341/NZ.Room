7a7673233e279aa4ec697985fe418347c11d8ca7
Vlad Boyko <40204913+DonSelester@users.noreply.github.com>
2025-04-18 08:50:52 +0300

    home page

--- nz_room/settings.py
+++ nz_room/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,15 +55,20 @@ ROOT_URLCONF = 'nz_room.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'nz_room', 'templates'),],
+        'APP_DIRS': '',
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
             ],
+            'loaders':[
+                'django.template.loaders.filesystem.Loader',
+            ]
         },
     },
 ]
