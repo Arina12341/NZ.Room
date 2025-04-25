e210dbe8f1934dfdb41e36a2ca68c7c5a9d8b41e
Vlad Boyko <40204913+DonSelester@users.noreply.github.com>
2025-04-25 12:02:56 +0300

    add my page

--- nz_room/settings.py
+++ nz_room/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'nz_room', 'templates'),],
-        'APP_DIRS': '',
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -66,9 +67,9 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
             ],
-            'loaders':[
-                'django.template.loaders.filesystem.Loader',
-            ]
+            # 'loaders':[
+            #     'django.template.loaders.filesystem.Loader',
+            # ]
         },
     },
 ]
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "accounts.User"
