acb759c6190822bf0da43de414b81d369fdf4069
Arina12341 <Lysenko12arina@gmail.com>
2025-04-26 11:06:20 +0300

    add tasks status

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,26 @@
 
 from django.db import models
+
+
+class TASK_STATUS:
+    NEW='new'
+    IN_PROGRESS='in_progress'
+    NOT_DONE='not_done'
+    DONE='done'
+    CHOICES=[
+        (NEW,'New')
+        (IN_PROGRESS,'In progress')
+        (NOT_DONE,'Not done')
+        (DONE,'Done')
+    ]
+
+
 class Task(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField()
     end_date=models.DateField()
-    
+    status=models.CharField(max_length=50,choices=TASK_STATUS.CHOICES,default=TASK_STATUS.NEW)
+
     def _str_(self):
         return self.title 
 
