192437bf124bd57649e52662909e1bb8dce810e0
KirilDy <dyakovkirusha1@gmail.com>
2025-04-26 11:07:51 +0300

    add_EventTask

--- events/models.py
+++ events/models.py
@@ -1,16 +1,34 @@
 from django.db import models
 from accounts.models import User
+from tasks.models import Task
+
+
+class EVENT_TYPES:
+    EXAM = 'exam'
+    TEST = 'test'
+    SCHOOL_EVENTS = 'school_events'
+    PARENT_TEACHER_MEETING = 'parent_teacher_meeting'
+    PERSONAL_EVENTS = 'personal_events'
+
+    choices=[
+        [EXAM, 'Exam'],
+        [TEST, 'Test'],
+        [SCHOOL_EVENTS, 'School Events'],
+        [PARENT_TEACHER_MEETING,'Parent-Teacher meeting'],
+        [PERSONAL_EVENTS,'Personal events']
+    ]
+
 
 class Event(models.Model):
-    typeEvent = models.CharField(max_length=50, choices=[['exam', 'Exaxm'],['test', 'Test'],['school_events', 'School Events'],['parent-teacher_meeting','Parent-Teacher meeting'],['personal_events','Personal events']])
-    title = models.TextField(max_length=25)
+    typeEvent = models.CharField(max_length=25, choices=EVENT_TYPES.choices, default=EVENT_TYPES.SCHOOL_EVENTS)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    duration = models.IntegerField()
-    location = models.TextField()
+    end_date = models.DateTimeField(blank=True)
+    duration = models.IntegerField(blank=True, default=15)
+    location = models.TextField(blank=True, default='')
     users = models.ManyToManyField(User)
-    # task = 
+    task = models.ForeignKey(Task, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
