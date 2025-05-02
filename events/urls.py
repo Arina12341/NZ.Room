3e04ea2bfbf6429aec8b77a3be7b9971d436008a
Arina12341 <Lysenko12arina@gmail.com>
2025-05-02 12:31:18 +0300

    add event_detail

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from events.views import events_list
+from events.views import events_list, event_detail
 urlpatterns = [
-    path('list/', events_list)
+    path('list/', events_list),
+    path('detail/<int:pk>/', event_detail)
 ]
--- events/views.py
+++ events/views.py
@@ -6,3 +6,9 @@ def events_list(request):
         request, 'events/events_calendar.html',
         {'events': events}
     )
+def event_detail(request, pk):
+    event=Event.objects.get(id=pk)
+    return render(
+        request, 'events/event_detail.html',
+        {'event': event}
+    )
